720feab8d91e9948b40f27d60b3b85be1b89359c
hwarim <rmrmhrm@gmail.com>
2024-10-08 19:03:16 +0900

    spring initializr

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "insight-into-me-api"
---
+++ src/test/kotlin/side/hwarim/insight_into_me/InsightIntoMeApplicationTests.kt
@@ -0,0 +1,13 @@
+package side.hwarim.insight_into_me
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InsightIntoMeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
